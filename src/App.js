8cf1e9deb7920e0a52279a2cf8976ac9b961dc85
Sasha <georgefbrockman@gmail.com>
2024-01-21 13:53:08 -0600

    adds html and variables to Admin and adds Admin to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { WeatherProvider } from './components/WeatherContext';
 import HourByHour from './pages/HourByHour';
 import CurrentWeather from './pages/CurrentWeather';
+import Admin from './pages/Admin';
 
 function App() {
   return (
     <>
       <h1>Welcome</h1>
+      <Admin></Admin>
       <WeatherProvider>
         <CurrentWeather/>
         <HourByHour/>
---
+++ src/pages/Admin.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function Admin(){
+    const [time, setTime] = useState("");
+    const [temp, setTemp] = useState(0);
+    const [clouds, setClouds] = useState("");
+    const [precip, setPrecip] = useState(false);
+
+    return(
+        <div>
+            <h1>Admin</h1>
+            <form>
+                <label for="time">Time: </label>
+                <div>
+                    <input 
+                        id="time"
+                        type="text"
+                        name="time"
+                        value={time}
+                        onChange={handleTime}
+                    />
+                </div>
+                <label for="temp">Temperature: </label>
+                <div>
+                    <input
+                        id="temp"
+                        type="number"
+                        name="temp"
+                        value={temp}
+                        onChange={handleTemp}
+                    />
+                </div>
+                <label for="clouds">Cloud cover: </label>
+                <div>
+                    <select id="clouds" name="clouds" onChange={handleClouds}>
+                        <option value="clear">Clear</option>
+                        <option value="partial">Partially Cloudy</option>
+                        <option value="cloudy">Cloudy</option>
+                    </select>
+                </div>
+                <label for="precip">Rain chance: </label>
+                <div>
+                    <input 
+                        id="precip"
+                        type="checkbox"
+                        name="precip"
+                        value={precip}
+                        onChange={hanldePrecip}
+                    />
+                </div>
+                <button type="submit">Add forecast</button>
+            </form>
+        </div>
+    );
+}
+
+export default Admin;
