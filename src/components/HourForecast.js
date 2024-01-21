b7ac9b2791042bf6a600a1cfb8788b04cbd0085b
Sasha <georgefbrockman@gmail.com>
2024-01-20 21:52:15 -0600

    changes div element to li element in HourForecast

--- src/components/HourForecast.js
+++ src/components/HourForecast.js
@@ -2,12 +2,12 @@ import React from "react";
 
 function HourForecast({ hour }){
     return(
-        <div class="hour">
+        <li class="hour">
             <h2>{hour.time}</h2>
             <p>{hour.temp}</p>
             <p>{hour.cloud-cover}</p>
             <p>{hour.precip ? "Rain" : "No rain"}</p>
-        </div>
+        </li>
     )
 }
 
