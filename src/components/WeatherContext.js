1993e8241bec0497b02408beb68ac445dbdb2833
Sasha <georgefbrockman@gmail.com>
2024-01-20 21:37:45 -0600

    updates WeatherContext

--- src/components/WeatherContext.js
+++ src/components/WeatherContext.js
@@ -1,19 +1,20 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react';
 
 const WeatherContext = useContext();
 
 function WeatherProvider(){
-    const [weather, setWeather] = useState([])
+    const [weather, setWeather] = useState([]);
+    const forecast = [weather, setWeather];
 
     useEffect(() => {
         fetch("http://localhost:3000/weather")
         .then(r => r.json())
         .then(r => setWeather(r))
-    })
+    },[]);
 
     return(
-        <WeatherContext.Provider value={weather}>{children}</WeatherContext.Provider>
-    )
+        <WeatherContext.Provider value={forecast}>{children}</WeatherContext.Provider>
+    );
 }
 
-export {WeatherProvider, WeatherContext}
+export {WeatherProvider, WeatherContext};
