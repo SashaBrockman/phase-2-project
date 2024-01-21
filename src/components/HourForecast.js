1fc9ccb50184b857e88a2872096d2efcd9c5c207
Sasha <georgefbrockman@gmail.com>
2024-01-20 22:02:51 -0600

    changes cloud-cover key to clouds for use

--- src/components/HourForecast.js
+++ src/components/HourForecast.js
@@ -5,7 +5,7 @@ function HourForecast({ hour }){
         <li class="hour">
             <h2>{hour.time}</h2>
             <p>{hour.temp}</p>
-            <p>{hour.cloud-cover}</p>
+            <p>{hour.clouds}</p>
             <p>{hour.precip ? "Rain" : "No rain"}</p>
         </li>
     )
