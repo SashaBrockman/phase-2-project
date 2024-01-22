c92c7444ffdd901dc5280f286955a7fa65d329a8
Sasha <georgefbrockman@gmail.com>
2024-01-21 18:17:26 -0600

    sets us routing in App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 import { WeatherProvider } from './components/WeatherContext';
 import HourByHour from './pages/HourByHour';
 import CurrentWeather from './pages/CurrentWeather';
 import Admin from './pages/Admin';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <h1>Welcome</h1>
       <WeatherProvider>
-        <Admin/>
-        <CurrentWeather/>
-        <HourByHour/>
+        <Outlet/>
       </WeatherProvider>
     </>
   );
--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,26 @@
 import CurrentWeather from "./pages/CurrentWeather";
 import HourByHour from "./pages/HourByHour";
 import Admin from "./pages/Admin"
+import App from "./App";
 
 const routes = [
     {
         path: "/",
-        element: <CurrentWeather/>
-    },
-    {
-        path: "/hourly",
-        element: <HourByHour/>
-    },
-    {
-        path: "/admin",
-        element: <Admin/>
+        element: <App/>,
+        children: [
+            {
+                path: "/",
+                element: <CurrentWeather/>
+            },
+            {
+                path: "/hourly",
+                element: <HourByHour/>
+            },
+            {
+                path: "/admin",
+                element: <Admin/>
+            }
+        ]
     }
 ];
 
