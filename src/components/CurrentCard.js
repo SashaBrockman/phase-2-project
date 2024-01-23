9556884ce3c393b3d8252120689e219ca7107560
Sasha <georgefbrockman@gmail.com>
2024-01-23 17:47:27 -0600

    adds css styling to CurrentWeather page and entire doc

--- src/components/CurrentCard.js
+++ src/components/CurrentCard.js
@@ -1,14 +1,16 @@
 import React from "react";
+import "./CurrentCard.css";
 
 function CurrentCard({current}){
     if(typeof current === "undefined") return <h2>Loading</h2>
 
     return(
         <div className="current">
-            <h2>{current.time}</h2>
-            <p>Temperature: {current.temp} degrees</p>
-            <p>Cloud cover: {current.clouds}</p>
-            <p>{current.precip ? "Raining" : "No rain"}</p>
+            <h1 style={{textAlign: "center"}}>Current Weather</h1>
+            <h2 style={{textAlign: "center"}}>{current.time}</h2>
+            <h3 style={{paddingTop: 5, paddingLeft: 10}}>Temperature: {current.temp} degrees</h3>
+            <h3 style={{paddingTop: 5, paddingLeft: 10}}>Cloud cover: {current.clouds}</h3>
+            <h3 style={{paddingTop: 5, paddingLeft: 10}}>{current.precip ? "Raining" : "No rain"}</h3>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import routes from './routes';
 
 const router = createBrowserRouter(routes)
 
+document.body.style = 'background: #2784db; font-family: Verdana, Geneva, Tahoma, sans-serif;';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<RouterProvider router={router}/>);
