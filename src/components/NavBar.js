c7a19835114567c44cdbb8e8af13d6a561c7817a
Sasha <georgefbrockman@gmail.com>
2024-01-23 16:08:48 -0600

    adds css styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,9 @@
 import { NavLink } from "react-router-dom";
+import "./NavBar.css"
 
 function NavBar(){
     return(
-        <nav className="navbar">
+        <nav className="nav-bar">
             <NavLink to="/hourly" className={"nav-link"}>Hourly Forecast</NavLink>
             <NavLink to="/" className={"nav-link"}>Current Weather</NavLink>
             <NavLink to="/admin" className={"nav-link"}>Admin</NavLink>
